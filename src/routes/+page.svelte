a42bf60c5a56cb1e9d4c0c7cfb05f8e79ebeaf5b
D3monizer <vysmol@gmail.com>
2025-06-04 22:05:45 +0300

    feat: add state and validation for textareas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,29 @@
+<script lang="ts">
+import validatePEM from '$lib/validatePEM';
+
+let key = $state('');
+let keyIsValid = $state<null | boolean>(null);
+let cert = $state('');
+let keyIsValid = $state<null | boolean>(null);
+</script>
+
 <h1>PEM Bunlder</h1>
 <p>Pass key and certificate to bundle them into PFX</p>
 
-<textarea name="key" aria-invalid="true" aria-describedby="key-helper">
+<textarea
+        bind:value={() => key, (v) => validatePEM(v)}
+        name="key"
+        aria-invalid="false"
+        aria-describedby="key-helper"
+        required>
 </textarea>
 <small id="key-helper">Looks good!</small>
 
 <textarea
+        bind:value={() => cert, (v) => validatePEM(v)}
         name="certificate"
-        aria-invalid="true"
-        aria-describedby="certificate-helper">
+        aria-invalid={null}
+        aria-describedby="certificate-helper"
+        required>
 </textarea>
 <small id="certificate-helper">Looks good!</small>
