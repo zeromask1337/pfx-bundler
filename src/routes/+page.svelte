f8c986dc13699fea5d380478400293a9ed71dfb1
D3monizer <vysmol@gmail.com>
2025-06-04 13:37:21 +0300

    feat: add textareas for key and cert

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
 <h1>PEM Bunlder</h1>
 <p>Pass key and certificate to bundle them into PFX</p>
+
+<textarea name="key" aria-invalid="true" aria-describedby="key-helper">
+</textarea>
+<small id="key-helper">Looks good!</small>
+
+<textarea
+        name="certificate"
+        aria-invalid="true"
+        aria-describedby="certificate-helper">
+</textarea>
+<small id="certificate-helper">Looks good!</small>
